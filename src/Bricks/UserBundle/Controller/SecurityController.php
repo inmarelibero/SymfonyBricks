c986962896ab98b6788184320cbce3d06d4a58ac
inmarelibero <inmarelibero@gmail.com>
2013-12-04 19:49:02 +0100

    homepage restyling

--- src/Bricks/UserBundle/Controller/SecurityController.php
+++ src/Bricks/UserBundle/Controller/SecurityController.php
@@ -3,19 +3,41 @@
 namespace Bricks\UserBundle\Controller;
 
 use FOS\UserBundle\Controller\SecurityController as BaseController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class SecurityController extends BaseController
 {
     /**
-     * print the code for the modal login box
+     * Print the code for the modal login box
+     * @Template
      */
     public function _modalLoginAction($targetPath = null)
     {
-        $csrfToken = $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate');
-        
-        return $this->container->get('templating')->renderResponse('BricksUserBundle:Security:_modalLogin.html.twig', array(
+        $csrfToken = $this->getCsrfToken();
+
+        return array(
             'csrf_token' => $csrfToken,
             'targetPath' => $targetPath
-        ));
+        );
+    }
+
+    /**
+     * Print the code for the login form in homepage
+     *
+     * @Template
+     */
+    public function _homepageLoginAction($targetPath = null)
+    {
+        $csrfToken = $this->getCsrfToken();
+
+        return array(
+            'csrf_token' => $csrfToken,
+            'targetPath' => $targetPath
+        );
+    }
+
+    private function getCsrfToken()
+    {
+        return $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate');
     }
 }
