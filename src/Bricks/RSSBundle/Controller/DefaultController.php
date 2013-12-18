6221164d282d262da6d7244c4bf4f8eff1fe467a
inmarelibero <inmarelibero@gmail.com>
2013-12-18 22:10:40 +0100

    [Insight] Unused use statement should be avoided (DefaultController.php#L7)
    closes #106

--- src/Bricks/RSSBundle/Controller/DefaultController.php
+++ src/Bricks/RSSBundle/Controller/DefaultController.php
@@ -4,7 +4,6 @@ namespace Bricks\RSSBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
