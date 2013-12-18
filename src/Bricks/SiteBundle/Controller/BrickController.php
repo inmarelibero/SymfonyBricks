ced4bbd1de2aa7330f782605109aed5b5552d381
inmarelibero <inmarelibero@gmail.com>
2013-12-18 21:45:23 +0100

    [Insight] Unused use statement should be avoided (Controller/BrickController.php#L7)
    closes #68

--- src/Bricks/SiteBundle/Controller/BrickController.php
+++ src/Bricks/SiteBundle/Controller/BrickController.php
@@ -3,7 +3,6 @@
 namespace Bricks\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
