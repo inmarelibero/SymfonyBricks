23d74ba1bad2288482c29cbe56a5b15dfbc31b6a
inmarelibero <inmarelibero@gmail.com>
2013-12-18 21:42:19 +0100

    [Insight] Unused use statement should be avoided (ButtonModalMessageController.php#L12)
    closes #63

--- src/Bricks/MessageBundle/Controller/ButtonModalMessageController.php
+++ src/Bricks/MessageBundle/Controller/ButtonModalMessageController.php
@@ -8,7 +8,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use JMS\SecurityExtraBundle\Annotation\Secure;
 
 use Bricks\SiteBundle\Entity\Brick;
 
